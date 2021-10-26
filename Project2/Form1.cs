af22b1d94638858d7438c9858ec795462091e8b2
Nam Nguyen <namnguyen0905@outlook.com>
2021-10-26 14:38:41 -0400

    Revert "Add basic functions for onClick of HIT, STAND, and PLAY button"
    This reverts commit 18b6382267808ce6d3219b1b7c37650973485879.

--- Project2/Form1.cs
+++ Project2/Form1.cs
@@ -12,51 +12,9 @@ namespace Project2
 {
     public partial class Form1 : Form
     {
-        aDeckofCards deck = new aDeckofCards();
-
         public Form1()
         {
             InitializeComponent();
-
-            totalMoney.Text = "100";
-        }
-
-        private void playButton_Click(object sender, EventArgs e)
-        {
-            // Draw 2 cards for each player
-            aCard firstCard = deck.Draw();
-            aCard secondCard = deck.Draw();
-            // TODO: display player's cards
-
-            // Draw a card for the dealer
-            aCard card = deck.Draw();
-            // TODO: display dealer's card
-        }
-
-        private void hitButton_Click(object sender, EventArgs e)
-        {
-            // Draw a card
-            aCard card = deck.Draw();
-            // TODO: display player's card
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            // Draw until dealer's hand is > 17
-            while (false)   // TODO: Add condition for calculation 
-            {
-                aCard card = deck.Draw();
-                // TODO: display dealer's card
-            }
-
-            // Calculate outcome
-            if (true)   // TODO: if player wins
-            {
-                totalMoney.Text = (int.Parse(totalMoney.Text) + 15).ToString();
-            } else if (true) // TODO: if lose
-            {
-                totalMoney.Text = (int.Parse(totalMoney.Text) - 10).ToString();
-            }
         }
     }
 }
--- Project2/aDeckofCards.cs
+++ Project2/aDeckofCards.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace Project2
 {
-    public class aDeckofCards : IDrawCard
+    public class aDeckofCards
     {
-        private List<aCard> deck = new List<aCard>();
+        private readonly List<aCard> deck = new List<aCard>();
 
         public aDeckofCards()
         {
@@ -16,18 +15,6 @@ namespace Project2
 
                 deck.Add(new aCard(value, face, null));
             }
-        }
-
-        public aCard Draw()
-        {
-            Random r = new Random();
-
-            int val = r.Next(0, deck.Count);
-
-            aCard drawnCard = deck[val];
-            deck.RemoveAt(val);
-
-            return drawnCard;
         }
     }
 }
